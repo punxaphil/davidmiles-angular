f1f4745ab999006cb5077ce8a17246d3686fd01c
Johan Frick <johan.frick@mobenga.com>
2017-12-29 23:55:18 +0100

    clean data branch

--- docker/bash.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker container run -it --rm -v $(pwd):/opt -w /opt teracy/angular-cli bash
--- docker/build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker container run --rm -v $(pwd):/opt -w /opt teracy/angular-cli ng build -op docs
