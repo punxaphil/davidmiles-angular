e59753912e1311c4a7585f1e55319ba139d936a5
Johan Frick <johan.frick@gmail.com>
2018-01-02 15:34:43 +0100

    Only deploy from master

---
+++ .travis-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  export SSHPASS=$SSH_PASS
+  sshpass -e rsync --archive --delete --chmod=Du=rwx,go=rx,Fu=rwx,og=rx "dist/" "$SSH_USER@ssh.davidmiles.se:/www/angular/"
+fi
