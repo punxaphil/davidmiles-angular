ff6dd9d9ab34fd873c704f06b780d6eb50106167
Johan Frick <johan.frick@gmail.com>
2018-04-07 23:30:50 +0200

    Don't delete target file .htaccess when deploying

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -5,4 +5,4 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
   subdir="beta-$TRAVIS_BRANCH"
 fi
 export SSHPASS=$SSH_PASS
-sshpass -e rsync --archive --delete --exclude /www/beta-* --chmod=Du=rwx,go=rx,Fu=rwx,og=rx "dist/" "$SSH_USER@ssh.davidmiles.se:/www/$subdir"
+sshpass -e rsync --archive --delete --exclude /www/beta-* --exclude .htaccess --chmod=Du=rwx,go=rx,Fu=rwx,og=rx "dist/" "$SSH_USER@ssh.davidmiles.se:/www/$subdir"
